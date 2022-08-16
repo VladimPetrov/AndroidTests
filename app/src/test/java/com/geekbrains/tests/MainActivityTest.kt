520aa0c35550da3dda7655e877ce2e4e6a2a9e14
Vladimir Petrov <vl.pva@yandex.ru>
2022-08-16 20:32:13 +0300

    add tests on MainActivity

---
+++ app/src/test/java/com/geekbrains/tests/MainActivityTest.kt
@@ -0,0 +1,100 @@
+package com.geekbrains.tests
+
+import android.content.Context
+import android.os.Build
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.lifecycle.Lifecycle
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.geekbrains.tests.view.details.DetailsActivity
+import com.geekbrains.tests.view.search.MainActivity
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertNotNull
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+
+@RunWith(AndroidJUnit4::class)
+@Config(sdk = [Build.VERSION_CODES.O_MR1])
+class MainActivityTest {
+
+    private lateinit var scenario: ActivityScenario<MainActivity>
+    private lateinit var context: Context
+
+    @Before
+    fun setup() {
+        scenario = ActivityScenario.launch(MainActivity::class.java)
+        context = ApplicationProvider.getApplicationContext()
+    }
+
+    @Test
+    fun activity_AssertNotNull() {
+        scenario.onActivity {
+            assertNotNull(it)
+        }
+    }
+
+    @Test
+    fun activity_IsResumed() {
+        assertEquals(Lifecycle.State.RESUMED, scenario.state)
+    }
+
+    @Test
+    fun activityEditTextView_NotNull() {
+        scenario.onActivity {
+            val searchEditTextView = it.findViewById<EditText>(R.id.searchEditText)
+            assertNotNull(searchEditTextView)
+        }
+    }
+
+    @Test
+    fun activityEditTextView_HasHint() {
+        scenario.onActivity {
+            val searchEditTextView = it.findViewById<EditText>(R.id.searchEditText)
+            assertEquals("Enter keyword e.g. android", searchEditTextView.hint)
+        }
+    }
+
+    @Test
+    fun activityTextView_IsVisible() {
+        scenario.onActivity {
+            val searchEditTextView = it.findViewById<EditText>(R.id.searchEditText)
+            assertEquals(View.VISIBLE, searchEditTextView.visibility)
+        }
+    }
+
+    @Test
+    fun activityDetailsButton_NotNull() {
+        scenario.onActivity {
+            val detailsActivityButton = it.findViewById<Button>(R.id.toDetailsActivityButton)
+            assertNotNull(detailsActivityButton)
+        }
+    }
+
+    @Test
+    fun activityDetailsButton_HasHint() {
+        scenario.onActivity {
+            val detailsActivityButton = it.findViewById<Button>(R.id.toDetailsActivityButton)
+            assertEquals("to details", detailsActivityButton.text)
+        }
+    }
+
+    @Test
+    fun activityDetailsButton_IsVisible() {
+        scenario.onActivity {
+            val detailsActivityButton = it.findViewById<Button>(R.id.toDetailsActivityButton)
+            assertEquals(View.VISIBLE, detailsActivityButton.visibility)
+        }
+    }
+
+    @After
+    fun close() {
+        scenario.close()
+    }
+}
